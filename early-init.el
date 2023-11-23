0b5e9e6cb6546fda3d58f86ff271d4e8617bbfa4
Ismail Boukhssibi <iboukhss@student.42luxembourg.lu>
2023-11-23 11:52:14 +0100

    simple theme and looks

--- early-init.el
+++ early-init.el
@@ -1,2 +1,3 @@
+(setq inhibit-startup-screen t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; Custom file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (and custom-file
+	   (file-exists-p custom-file))
+  (load custom-file nil :nomessage))
+
 ;; Load lisp files
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
@@ -6,3 +12,16 @@
 (load "string.el")
 (load "comments.el")
 (load "header.el")
+
+;; Package
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(eval-when-compile
+  (require 'use-package))
+
+;; Theme
+(use-package modus-themes
+  :ensure t
+  :config (load-theme 'modus-vivendi t))
