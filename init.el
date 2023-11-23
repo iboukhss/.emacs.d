652493e62bdba5739276cdbe3546a994af49b44e
Ismail Boukhssibi <iboukhss@student.42luxembourg.lu>
2023-11-23 14:24:07 +0100

    fix indentation

--- init.el
+++ init.el
@@ -25,3 +25,16 @@
 (use-package modus-themes
   :ensure t
   :config (load-theme 'modus-vivendi t))
+
+;; Programming
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(column-number-mode 1)
+
+;; C
+(defun ism/setup-c ()
+  (local-set-key (kbd "TAB") 'tab-to-tab-stop)
+  (setq-default c-basic-offset 4
+		tab-width 4
+		indent-tabs-mode t))
+
+(add-hook 'c-mode-hook 'ism/setup-c)
